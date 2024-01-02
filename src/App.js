ba62791cf53188516cf9685f9c458181e08d7aac
Walee <walz.e3@gmail.com>
2024-01-02 14:49:52 +0000

    New Buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import {extendTheme, ChakraProvider, useColorMode, Button, Box, Card, Image  } from '@chakra-ui/react'
+import {extendTheme, ChakraProvider, useColorMode, Button, ButtonGroup, Box, Card, Image, IconButton, Center, CardBody  } from '@chakra-ui/react'
+import { MoonIcon } from '@chakra-ui/icons'
 import bookSlug from './pb.env';
+import bookLogo from './bookLogo.png'; 
 
 const App = () => {
   const [currentHadithIndex, setCurrentHadithIndex] = useState(0);
   const [hadiths, setHadiths] = useState([]);
+  const [showArabic, setShowArabic] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const bookSlugs = ['sahih-bukhari', 'al-tirmidhi']; // Array of book slugs
@@ -18,9 +21,9 @@ const App = () => {
 function ThemeToggleButton() {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
-      <Button onClick={toggleColorMode}>
+      <MoonIcon boxSize={6} onClick={toggleColorMode}>
         Switch to {colorMode === 'light' ? 'Dark' : 'Light'} Mode
-      </Button>
+      </MoonIcon>
     );
 }
 
@@ -30,6 +33,16 @@ const theme = extendTheme({
     useSystemColorMode: true, // Use the system color mode
   },
 });
+
+const toggleArabic = () => {
+  setShowArabic(!showArabic);
+};
+
+const refreshPage = () => {
+  window.location.reload();
+};
+
+
 //${process.env.REACT_APP_HADITH_API_KEY}
 useEffect(() => {
   const apiUrl = `https://www.hadithapi.com/api/hadiths?book=${currentBookSlug}&apiKey=$2y$10$vPGICR6blTNtOzixZkeeAsf0DXgGr8JyEg4CTyXfC9oVeDdc5QW`;
@@ -64,9 +77,8 @@ useEffect(() => {
 
   return (
     <ChakraProvider theme={theme}>
-       <Box p={6} m="auto" maxW="container.md" textalign="center">
+       <Box p={6} m="auto" maxW="container.md" >
         <Card>
-      
     <div>
       {isLoading ? (
         <p>Loading...</p>
@@ -74,19 +86,30 @@ useEffect(() => {
         <p>Error loading data: {error.message}</p>
       ) : (
         <div>
-
         <Card>
+          <CardBody>
           {console.log('Current index:', currentHadithIndex)} {/* Debugging */}
           {console.log('Current hadith:', hadiths[currentHadithIndex])} {/* Debugging */}
           <p>{hadiths[currentHadithIndex]?.bookSlug || 'No book'}</p>
           <br></br>
-          <p>{hadiths[currentHadithIndex]?.hadithEnglish || 'No English text available'}</p>
+          <Center><p>{hadiths[currentHadithIndex]?.englishNarrator || 'No book'}</p>
+          </Center><br></br>
+          <p textalign="center">{hadiths[currentHadithIndex]?.hadithEnglish || 'No English text available'}</p>
+          <br></br>
+          {showArabic && <p>{hadiths[currentHadithIndex]?.hadithArabic || 'No Arabic text available'}</p>}
+          </CardBody>
           </Card>
           <br></br>
           <Box textAlign="center">
+          <ButtonGroup variant='outline' size= 'md' spacing='5'>
+          <IconButton aria-label='Search database' icon={<ThemeToggleButton />} /> 
+          <Button onClick={toggleArabic}>{showArabic ? 'Hide Arabic' : 'Show Arabic'}</Button>
           <Button onClick={showNextHadith}>Show Next Hadith</Button>
-          <ThemeToggleButton />
-          <Image src ="quoteapp/src/bookLogo.png"></Image>
+          </ButtonGroup>
+          <Center>
+          <div onClick={refreshPage}><Image src ={bookLogo} alt='logo' width={["100px", "150px", "200px"]} textalign="center"></Image>
+          </div>
+          </Center>
           </Box>
         </div>
       )}
